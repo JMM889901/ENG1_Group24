0a0075910865bd444c5a349b6c052c391c695121
JoO0oss <95944691+JoO0oss@users.noreply.github.com>
2022-12-05 13:41:53 +0000

    Small UI/colour and variable name change.
    Not sure whether these two files were affected by that, but they're getting updated anyway.

--- libGDX/core/src/group24/piazzapanic/PiazzaPanic.java
+++ libGDX/core/src/group24/piazzapanic/PiazzaPanic.java
@@ -1,33 +1,53 @@
 package group24.piazzapanic;
 
+//import java.util.HashMap;
+import java.util.ArrayList;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
+
+import group24.piazzapanic.ui.Menu;
+import group24.piazzapanic.ui.MenuFactory;
+import group24.piazzapanic.Base;
+import group24.piazzapanic.ui.FontHandler;
 
 public class PiazzaPanic extends ApplicationAdapter {
-	SpriteBatch batch;
 	Texture img;
 
-	private String state = "menu_main";
+	private String state;
+	private ArrayList<Menu> menus = new ArrayList<Menu>();
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		// In desktop/build/src/group24/piazzapanic/DesktopLauncher.java:
+		// `config.setResizable(false)` has been added, so the user can't drag to resize.
+		// This line sets the size of the window, which shall not change during the game.
+		Gdx.graphics.setWindowedMode(Base.WINDOW_WIDTH, Base.WINDOW_HEIGHT);
+
+		FontHandler.create();
+
+		Base.batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		menus.add(MenuFactory.createMainMenu());
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0.8f, 0.8f, 0.8f, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Base.batch.begin();
+		//Base.batch.draw(img, 0, 0);
+		menus.get(0).render();
+		Base.batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		Base.batch.dispose();
+		FontHandler.dispose();
 		img.dispose();
 	}
 }
