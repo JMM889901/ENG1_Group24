db5778b0726e1abf6cc3f986d6e284201f72d068
JoO0oss <95944691+JoO0oss@users.noreply.github.com>
2022-12-05 13:19:52 +0000

    Create Wigit.java
    This file has been created in this manner, via the GitHub website, to create the folder structure to upload the other files straight into this directory.

---
+++ libGDX/core/src/group24/piazzapanic/ui/Wigit.java
@@ -0,0 +1,27 @@
+package group24.piazzapanic.ui;
+
+import group24.piazzapanic.maths.Vector2;
+import group24.piazzapanic.maths.Vector2i;
+
+public abstract class Wigit {
+    protected String wigitID;
+    protected Vector2i location;
+
+    public Wigit(String wigitID, Vector2 relativeLocation) {
+        this.wigitID = wigitID;
+        this.location = relativeLocation.translateToAbsoluteLocation();
+    }
+
+    /** Activated when the wigit is clicked, requires implementation by child classes. */
+    public void click() {
+        System.out.println("Unimplemented Wigit click method has been called!");
+    }
+    
+    public void update() {
+
+    }
+
+    public void render() {
+
+    }
+}
