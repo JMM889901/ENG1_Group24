c00ee32d1edf8f414620c1a11bdf53c189a970dd
JoO0oss <95944691+JoO0oss@users.noreply.github.com>
2022-12-05 12:59:22 +0000

    Add files via upload
    Added `config.setResizable(false);` so that the game always runs at the resolution given to it by `Gdx.graphics.setWindowedMode(Base.WINDOW_WIDTH, Base.WINDOW_HEIGHT);` in PiazzaPanic.java.
    
    Also changed the window's title/caption to "Piazza Panic" from "piazzapanic".

--- libGDX/desktop/src/group24/piazzapanic/DesktopLauncher.java
+++ libGDX/desktop/src/group24/piazzapanic/DesktopLauncher.java
@@ -9,7 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("piazzapanic");
+		config.setTitle("Piazza Panic");
+		config.setResizable(false);
 		new Lwjgl3Application(new PiazzaPanic(), config);
 	}
 }
