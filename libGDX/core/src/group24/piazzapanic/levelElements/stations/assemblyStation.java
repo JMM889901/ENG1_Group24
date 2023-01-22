50a636c9e9ba61013ee33353b8f61194b644b4db
Emily <ehd507@york.ac.uk>
2023-01-22 21:40:58 +0000

    make the errors less horrifying.

---
+++ libGDX/core/src/group24/piazzapanic/levelElements/stations/assemblyStation.java
@@ -0,0 +1,9 @@
+package group24.piazzapanic.levelElements.stations;
+
+import group24.piazzapanic.Base;
+
+public class assemblyStation extends Station{
+    public assemblyStation() {
+        super(Base.assemblyStationTexture);
+    }
+}
