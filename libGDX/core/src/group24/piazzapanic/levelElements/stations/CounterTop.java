a3c9c59166952e0b22c612040de19b9566ad14db
Emily <ehd507@york.ac.uk>
2023-01-23 16:51:07 +0000

    is this commit creepy or wet?

--- libGDX/core/src/group24/piazzapanic/levelElements/stations/CounterTop.java
+++ libGDX/core/src/group24/piazzapanic/levelElements/stations/CounterTop.java
@@ -3,22 +3,55 @@ package group24.piazzapanic.levelElements.stations;
 import group24.piazzapanic.Base;
 import group24.piazzapanic.Physics.Movable;
 import group24.piazzapanic.levelElements.Dish;
+import group24.piazzapanic.levelElements.Ingredient;
+
 import java.util.ArrayList;
 
 public class CounterTop extends Station {
 
-    ArrayList item = new ArrayList<>();
+    ArrayList<Movable> items = new ArrayList<Movable>();
+
     public CounterTop() {
         super(Base.counterTopTexture);
-        this.item = null;
+        this.items = null;
     }
 
     public CounterTop(Movable item) {
         super(Base.counterTopTexture);
-        this.item.add(item);
+        this.items.add(item);
     }
 
-    public void act(float delta){
-        
+    @Override
+    public boolean placeItem(Movable item) {
+        boolean hasDish = false;
+        if (this.items.size() > 0) { // If it currently has items on it.
+            for (Movable currentItem : this.items) { // If one of these is a dish.
+                if (currentItem instanceof Dish) {
+                    hasDish = true;
+                }
+            }
+        }
+        if (hasDish) {
+            this.items.add(item);
+            return true;
+        } else {
+            if (this.items.size() == 0){
+                this.items.add(item);
+                return true;
+            }
+        }
+        return false;
     }
+
+    @Override
+    public Movable takeItem(){
+        if(this.items.size() == 1){ // Nice, simple easy. just one item
+            return this.items.get(0);
+        }
+        if(this.items.size() > 1){
+            return this.items.get(this.items.size() - 1); // Return the last item 
+        }
+        return null; // countertop, her eyes enormous: you STEAL ITEM FROM countertop? you STEAL her ITEM like the criminal? oh! oh! jail for mother! jail for mother for One Thousand Years!!!!
+    }
+
 }
