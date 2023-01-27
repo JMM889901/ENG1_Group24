d3a61578cab8b0129c152791d611bafb805a501f
Emily <ehd507@york.ac.uk>
2023-01-27 15:02:34 +0000

    continuing work on javadoc etc

--- libGDX/core/src/group24/piazzapanic/levelElements/stations/CuttingStation.java
+++ libGDX/core/src/group24/piazzapanic/levelElements/stations/CuttingStation.java
@@ -1,27 +1,19 @@
 package group24.piazzapanic.levelElements.stations;
-
-import javax.print.event.PrintEvent;
-
 import com.badlogic.gdx.Gdx;
-
 import group24.piazzapanic.Base;
 import group24.piazzapanic.game.GameData;
 import group24.piazzapanic.game.Physics;
 
 public class CuttingStation extends Station {
-
     public CuttingStation() {
         super(Base.cuttingStationTexture);
     }
-
     @Override
     public void act(float delta) {
-
         if (super.item != null) {
             if (super.item.getIngredient() == null) {
                 return;
             }
-
             if (Gdx.input.isKeyPressed(Base.ACT_KEY) && GameData.player.getFacingStation() == this
                     && Physics.isNear(this, GameData.player)) {
                 if (super.item.getIngredient().getCuttingProgress() == 1) {
@@ -34,7 +26,6 @@ public class CuttingStation extends Station {
                 }
                 timeKeyHeld += delta;
                 if (timeKeyHeld > 3 && super.item.getIngredient().getCuttingProgress() == 0) {
-                    // Cutting is done! poggers
                     super.item.getIngredient().cut();
                     System.out.println("Cutting complete...");
                     timeKeyHeld = 0;
--- libGDX/core/src/group24/piazzapanic/levelElements/stations/ServingStation.java
+++ libGDX/core/src/group24/piazzapanic/levelElements/stations/ServingStation.java
@@ -1,19 +1,18 @@
 package group24.piazzapanic.levelElements.stations;
 
 import com.badlogic.gdx.graphics.Texture;
-
 import group24.piazzapanic.Physics.Movable;
 import group24.piazzapanic.game.Customer;
 import group24.piazzapanic.game.GameData;
 import group24.piazzapanic.levelElements.Dish;
 
-public class ServingStation extends Station{
+public class ServingStation extends Station {
 
     public ServingStation(Texture t) {
         super(t);
         this.item = null;
     }
-    
+
     @Override
     public boolean placeItem(Movable item) {
         if (item instanceof Dish) return serveOrder(item);
@@ -22,7 +21,7 @@ public class ServingStation extends Station{
 
     private boolean serveOrder(Movable dish) {
         for (Customer c : GameData.customers) {
-            if (dish == c.getOrder()){
+            if (dish == c.getOrder()) {
                 c.fulfillOrder();
                 return true;
             }
