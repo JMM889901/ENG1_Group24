9f5ba92826dd335cafedb5d87887e5ed1ccda23a
Emily <ehd507@york.ac.uk>
2023-02-01 11:51:16 +0000

    added

---
+++ _site/docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"group24.piazzapanic"},{"l":"group24.piazzapanic.game"},{"l":"group24.piazzapanic.levelElements"},{"l":"group24.piazzapanic.levelElements.stations"},{"l":"group24.piazzapanic.maths"},{"l":"group24.piazzapanic.Physics"},{"l":"group24.piazzapanic.ui"}]
---
+++ _site/libGDX/core/src/group24/piazzapanic/ui/FontHandler.java
@@ -0,0 +1,44 @@
+package group24.piazzapanic.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+
+/**
+ * Everything here applies gamewide and are resources to be used by objects in
+ * the game.
+ */
+public class FontHandler {
+    //public static final String FONT_PATH = "fonts/liberation-serif/LiberationSerif-Regular.ttf";
+    public static final String FONT_PATH = "fonts/ArcadeFont.ttf";
+    public static BitmapFont titleFormat;
+    public static BitmapFont subtitleFormat;
+    public static BitmapFont testFormat;
+    public static BitmapFont textButtonFormat;
+
+    public static void create() {
+        // Full disclosure, this bit of code is more or less copied from the
+        // documentation.
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(FONT_PATH));
+        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+        parameter.size = 72;
+        titleFormat = generator.generateFont(parameter);
+
+        parameter.size = 36;
+        subtitleFormat = generator.generateFont(parameter);
+
+        parameter.size = 24;
+        textButtonFormat = generator.generateFont(parameter);
+
+        parameter.size = 10;
+        testFormat = generator.generateFont(parameter);
+
+        generator.dispose();
+    }
+
+    public static void dispose() {
+        // Make sure all fonts created above are disposed of here.
+        titleFormat.dispose();
+    }
+}
